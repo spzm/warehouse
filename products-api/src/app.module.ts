f6f9de367031af165cc6a2ef9c57b982bdc58cff
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2021-06-02 23:27:33 +0200

    feat: add postgre connection configuration. configure docker compose to dev env

--- products-api/src/app.module.ts
+++ products-api/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+          validationSchema: Joi.object({
+              NODE_ENV: Joi.string()
+                  .valid('development', 'production')
+                  .default('development'),
+              PORT: Joi.number().default(3000),
+              DB_PG_HOST: Joi.string().required(),
+              DB_PG_PORT: Joi.string().required(),
+              DB_PG_USER: Joi.string().required(),
+              DB_PG_PASSWORD: Joi.string().required(),
+              DB_PG_DATABASE: Joi.string().required()
+          }),
+          validationOptions: {
+              abortEarly: true
+          }
+      }),
+      DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
