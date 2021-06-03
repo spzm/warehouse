a26643684563c41770a37b5aa7898a7646801649
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2021-06-04 00:53:56 +0200

    feat: fix unit tests

--- products-api/src/inventory-articles/tests/create-post-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CreateArticlesDto } from '../dto/create-articles-dto';
-
-describe('CreatePostDto', () => {
-  it('should be defined', () => {
-    expect(new CreateArticlesDto()).toBeDefined();
-  });
-});
--- products-api/src/inventory-articles/tests/inventory-articles.controller.spec.ts
+++ products-api/src/inventory-articles/tests/inventory-articles.controller.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { InventoryArticlesController } from '../inventory-articles.controller';
+import {Test, TestingModule} from '@nestjs/testing';
+import {InventoryArticlesController} from '../inventory-articles.controller';
+import {getRepositoryToken, TypeOrmModule} from "@nestjs/typeorm";
+import {Article} from "../entities/article.entity";
+import {Inventory} from "../entities/inventory.entity";
+import {Repository} from "typeorm";
+import {InventoryArticlesService} from "../inventory-articles.service";
 
 describe('ArticlesController', () => {
-  let controller: InventoryArticlesController;
+    let controller: InventoryArticlesController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [InventoryArticlesController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [InventoryArticlesController],
+            providers: [InventoryArticlesService, {
+                provide: getRepositoryToken(Article),
+                useClass: Repository
+            }, {
+                provide: getRepositoryToken(Inventory),
+                useClass: Repository
+            }],
+        }).compile();
 
-    controller = module.get<InventoryArticlesController>(InventoryArticlesController);
-  });
+        controller = module.get<InventoryArticlesController>(InventoryArticlesController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
